20cc00dbf5aed82121e01b6278a66d0c4eaf396b
tm4roon <tm998244353@gmail.com>
2025-11-28 17:48:22 +0900

    ADD: LLMによる修正が書かれたセクション

--- src/domain/diary/diary.py
+++ src/domain/diary/diary.py
@@ -16,7 +16,8 @@ class Diary(BaseModel):
 
     title: str = Field(..., description="日記のタイトル.")
     diary_date: date | None = Field(None, description="日記の日付")
-    entries: list[DiaryEntry] = Field(..., description="日記のエントリのリスト.")
+    original_entries: list[DiaryEntry] = Field(..., description="日記のエントリのリスト.")
+    revised_entries: list[DiaryEntry] = Field(..., description="LLMで修正した日記エントリ.")
 
 
 class DiaryFactory:
@@ -30,15 +31,21 @@ class DiaryFactory:
         diary_page = self.page_factory.from_notion(page)
 
         sections = self.split_by_heading1(children)
-        entries = []
+        original_entries = []
+        revised_entries = []
         for section in sections:
-            if self.is_entry_section(section):
-                entries = self.entry_factory.from_notion(section)
+            if self.is_original_entry_section(section):
+                original_entries = self.entry_factory.from_notion(section)
+                continue
+            if self.is_revised_entry_section(section):
+                revised_entries = self.entry_factory.from_notion(section)
+                continue
 
         return Diary(
             title=diary_page.properties.title,
             diary_date=diary_page.properties.diary_date,
-            entries=entries,
+            original_entries=original_entries,
+            revised_entries=revised_entries,
         )
 
     def split_by_heading1(self, children: list[dict]) -> Generator[list[dict], None, None]:
@@ -50,7 +57,7 @@ class DiaryFactory:
             t = offsets[i + 1] if i + 1 < len(offsets) else len(children)
             yield list(children[s:t])
 
-    def is_entry_section(self, blocks: list[dict]) -> bool:
+    def is_original_entry_section(self, blocks: list[dict]) -> bool:
         """'Entryのセクションかどうかを判定"""
         head_block = blocks[0]
         if head_block.get("type", "") != NotionBlockType.HEADING1:
@@ -65,3 +72,20 @@ class DiaryFactory:
         if plain_text.strip() != "Entry":
             return False
         return True
+
+
+    def is_revised_entry_section(self, blocks: list[dict]) -> bool:
+        """'Entryのセクションかどうかを判定"""
+        head_block = blocks[0]
+        if head_block.get("type", "") != NotionBlockType.HEADING1:
+            return False
+
+        heading = head_block.get(NotionBlockType.HEADING1, {})
+        rich_text = heading.get("rich_text", {})
+
+        plain_text = ""
+        for e in rich_text:
+            plain_text += e.get("plain_text", "")
+        if plain_text.strip().startswith("Revised"):
+            return True
+        return False
